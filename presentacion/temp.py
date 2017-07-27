3b82bcf03addec0f48b7c82320613cd2d680e5e4
APimentelA <apimentela@iingen.unam.mx>
2017-07-27 10:17:08 -0500

    modifiqué la preesentación para que ya no usara COMPENDIO, ahora solo usa el archivo mas grande, el plan es que al principio solo bajen lo que necesitan

--- presentacion/temp.py
+++ presentacion/temp.py
@@ -1,4 +1,4 @@
-carpeta_nombre="/home/alejandro/Escritorio/PaginaCurso/presentacion/Documentos/"
+carpeta_nombre="/home/alejandro/cursopln/presentacion/Documentos/"
 archivo_nombre="P_IFT_290216_73_Acc.txt"
 
 import os
@@ -6,35 +6,43 @@ import re
 import nltk
 
 #~ carpeta_nombre="C:\\Users\\user\\Desktop\\Documentos\\"
-archivo_nombre="P_IFT_290216_73_Acc.txt"
+archivo_nombre="DOF_P_IFT_291116_672_Acc.txt"
 #~ archivo_nombre="COMPENDIO.txt"
 
 with open(carpeta_nombre+archivo_nombre,"r") as archivo:
 	texto=archivo.read()
 	
-palabras_funcionales=nltk.corpus.stopwords.words("spanish")
+#~ palabras_funcionales=nltk.corpus.stopwords.words("spanish")
 
 tokens=nltk.word_tokenize(texto,"spanish")
-
-tokens_limpios=[]
-for token in tokens:
-	if token not in palabras_funcionales:
-		if len(token) > 1:
-			tokens_limpios.append(token)
-
-texto_limpio_nltk=nltk.Text(tokens_limpios)
-
-from nltk.tag import StanfordPOSTagger
-
-# Aquí obtenemos la lista de tokens en "tokens"
-
 texto_nltk=nltk.Text(tokens)
+texto_nltk.similar("artículo")
+print()
+texto_nltk.common_contexts(["artículo","instituto"])
 
-tagger="C:\\Users\\user\\Downloads\\...\\spanish.tagger"
-jar="C:\\Users\\user\\Downloads\\...\\stanford-postagger.jar"
+lista_palabras=["Instituto","Ley","Elija","ley"]
+texto_nltk.dispersion_plot(lista_palabras)
 
-etiquetador=StanfordPOSTagger(tagger,jar)
-etiquetas=etiquetador.tag(tokens)
 
-for etiqueta in etiquetas:
-	print(etiqueta)
+#~ tokens_limpios=[]
+#~ for token in tokens:
+	#~ if token not in palabras_funcionales:
+		#~ if len(token) > 1:
+			#~ tokens_limpios.append(token)
+
+#~ texto_limpio_nltk=nltk.Text(tokens_limpios)
+
+#~ from nltk.tag import StanfordPOSTagger
+
+#~ # Aquí obtenemos la lista de tokens en "tokens"
+
+#~ texto_nltk=nltk.Text(tokens)
+
+#~ tagger="C:\\Users\\user\\Downloads\\...\\spanish.tagger"
+#~ jar="C:\\Users\\user\\Downloads\\...\\stanford-postagger.jar"
+
+#~ etiquetador=StanfordPOSTagger(tagger,jar)
+#~ etiquetas=etiquetador.tag(tokens)
+
+#~ for etiqueta in etiquetas:
+	#~ print(etiqueta)

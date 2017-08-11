2281132a7773de1573ed8e4ca82011a9751278a8
APimentelA <apimentela@iingen.unam.mx>
2017-08-10 22:01:50 -0500

    clase 4

---
+++ plnPython/solucion4_1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+carpeta_nombre="Documentos\\"
+archivo_nombre="DOF_P_IFT_291116_672_Acc.txt"
+
+with open(carpeta_nombre+archivo_nombre,"r") as archivo:
+	lineas_lista=archivo.readlines()
+
+for linea in lineas_lista:
+	if linea.strip() == "":
+		break
+	print(linea)
---
+++ plnPython/solucion5.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+carpeta_nombre="Documentos\\"
+
+carpeta_salida="Solucion5"
+archivo_salida="UNION.txt"
+
+archivos_lista=os.listdir(carpeta_nombre)
+
+union=""
+for archivo_nombre in archivos_lista:
+	archivo=open(carpeta_nombre+archivo_nombre)
+	texto = archivo.read()
+	archivo.close()
+	union+=texto
+
+with open(carpeta_salida+archivo_salida,"w") as salida:
+	salida.write(union)
